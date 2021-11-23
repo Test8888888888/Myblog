b68aa4af5d66e1e1ef3807f4ed46032089343723
MeetYou <fuwei@xiaoyouzi.com>
2021-11-23 19:10:56 +0800

    修改样式

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
 from app.settings import DevelopmentConfig as Cfg
 
 class Config(Cfg):
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:"Fuwei123321."@120.25.77.192/Bingtang_blog?charset=utf8'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@120.25.77.192/Bingtang_blog?charset=utf8'
